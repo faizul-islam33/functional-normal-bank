1b63ae9efa8a6dfd0ecd3e00c2f5d6a43f3cc7b1
faizul-islam33 <pleasurehouse55@gmail.com>
2022-08-17 02:51:31 +0600

    functions

--- js/deposit.js
+++ js/deposit.js
@@ -1,3 +1,21 @@
+function getInputFieldValueById(inputFieldId) {
+    const inputField = document.getElementById(inputFieldId);
+    const inputFieldValueString = inputField.value;
+    const inputFieldValue = parseFloat(inputFieldValueString);
+    inputField.value = '';
+    return inputFieldValue;
+}
+
+
+function getElementValueById(elementId) {
+    const element = document.getElementById(elementId);
+    const elementValueString = element.innerText;
+    const value = parseFloat(elementValueString);
+    return value;
+}
+
+
+
 document.getElementById('btn-deposit').addEventListener('click', function () {
     /*
     1. get the element by id
@@ -5,4 +23,6 @@ document.getElementById('btn-deposit').addEventListener('click', function () {
     3. convert string value to a number
 
     */
+
+    const newDepositAmount = getInputFieldValueById('deposit-field');
 })
